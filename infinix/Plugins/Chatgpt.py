32b29a78d16f34b470a0ddb9d55b0179dbc565b4
Ankii14 <134953271+Ankii14@users.noreply.github.com>
2023-06-13 13:47:45 +0530

    Update Chatgpt.py

--- infinix/Plugins/Chatgpt.py
+++ infinix/Plugins/Chatgpt.py
@@ -1,15 +1,16 @@
-from telethon import events
-from .. import bot
-from telethon.tl.custom import Button 
-
-import asyncio
-import openai
-
-openai.api_key = " "
-#openai.api_key = openai_key
-
-@bot.on(events.NewMessage(incoming=True, pattern = "(?i)/ask"))
-async def _gpt(event):  
-  sender_id = event.sender_id 
-  gpt_msg = "hello i am your problem solver"
-  await bot.send_msg(sender_id, gpt_msg)
+from telethon import events 
+from .. import bot 
+import asyncio 
+import openai 
+  
+openai.api_key = "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" 
+ #openai.api_key = openai_key 
+  
+@bot.on(events.NewMessage(incoming=True, pattern = "(?i)/ask")) 
+async def _gpt(event): 
+   #if event.sender_id ==int(5370621967): 
+ await event.reply("yes! you are My Developer you developed me very well") 
+   # else: 
+    # await event.reply("you are user: nice to meet you") 
+  
+ await event.reply (event)
