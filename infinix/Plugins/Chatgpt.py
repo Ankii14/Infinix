7b779526b3ac86bf55b059209217b2f742a2579c
Ankii14 <134953271+Ankii14@users.noreply.github.com>
2023-06-13 13:48:18 +0530

    Update Chatgpt.py

--- infinix/Plugins/Chatgpt.py
+++ infinix/Plugins/Chatgpt.py
@@ -8,7 +8,7 @@ openai.api_key = "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"
   
 @bot.on(events.NewMessage(incoming=True, pattern = "(?i)/ask")) 
 async def _gpt(event): 
-   #if event.sender_id ==int(5370621967): 
+   #if event.sender_id ==int(): 
  await event.reply("yes! you are My Developer you developed me very well") 
    # else: 
     # await event.reply("you are user: nice to meet you") 
